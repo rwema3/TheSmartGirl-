f1b53c930a30c4dbd992fa5567f68a55d1c353e3
rwema3 <rwemadominik3@gmail.com>
2022-02-18 14:55:00 +0200

    girl App

--- lib/main - Copy.dart
+++ lib/main - Copy.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'Lato',
-        primarySwatch: Colors.deepOrange
+        primarySwatch: Colors.
 
 
 
