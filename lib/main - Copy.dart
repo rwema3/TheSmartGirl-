16170bcd0a06fc214cdd1d3039746ff443135327
rwema3 <rwemadominik3@gmail.com>
2022-02-18 15:13:32 +0200

    girl's App

--- lib/main - Copy.dart
+++ lib/main - Copy.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: fa
+      debugShowCheckedModeBanner: f
 
 
 
