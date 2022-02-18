0d6073d8acef1143057ff938002be831903d113e
rwema3 <rwemadominik3@gmail.com>
2022-02-18 15:11:25 +0200

    girl's App

--- lib/main - Copy.dart
+++ lib/main - Copy.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutte
+      title: 'Flut
 
 
 
